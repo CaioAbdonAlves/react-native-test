957b55113151fcf0f9589a894c043f8307d757f1
Caio Abdon <caio.alves54@etec.sp.gov.br>
2023-09-18 18:21:21 -0300

    feat: mock de tarefas

---
+++ constants/DefautTasks.ts
@@ -0,0 +1,12 @@
+export const defaultTasks = [{
+    title: "Grab some Pizza",
+    completed: true
+},
+{
+    title: "Do your workout",
+    completed: true
+},
+{
+    title: "Hangout with friends",
+    completed: false
+}]
